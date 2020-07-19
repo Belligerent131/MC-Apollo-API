6975a32a219f4f1ecafabd646a6d03929e01bbfc
BuildTools <unconfigured@null.spigotmc.org>
2020-07-18 22:29:31 -0700

    Added examples and helpful tips to graphql queries

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,5 +1,20 @@
 export default {
   Query: {
     testMessage: (): string => 'Hello World!',
+    numberSix() {
+      return 6;
+    },
+    numberSeven() {
+      return 7;
+    },
+    getNumber(_, args: any) {
+      return args.number;
+    },
   },
 };
+
+/**
+ * Resolvers are the interceptors of the graphql api. Think of queries as callable functions.
+ * Parameters are as followed (parent, args, context, info)
+ * parameters can be skipped using an underscore.
+ */
